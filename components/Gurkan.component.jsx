07db008e4dec73b5be156bb0cb6018f95d90676b
Dagdelen Gurkan <dagdelen.gurkan03@gmail.com>
2025-10-08 12:21:37 +0200

    Ajout du composant React GurkanComponent

--- components/Gurkan.component.jsx
+++ components/Gurkan.component.jsx
@@ -1,1 +1,12 @@
-code log("Gurkan a bien fait");
+
+const GurkanComponent = () => {
+  return (
+    <div>
+      <h1>Composant de Gurkan chargé ✅</h1>
+      <p>Ceci est mon composant React pour le projet NutriApp.</p>
+    </div>
+  );
+};
+
+export default GurkanComponent;
+
